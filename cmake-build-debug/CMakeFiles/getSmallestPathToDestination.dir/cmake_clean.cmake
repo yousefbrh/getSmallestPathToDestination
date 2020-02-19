f08212d235c9f77b3debc2d95ab9a726e990a89d
Yousef Borhani <yousefborhanii@gmail.com>
2020-02-19 13:58:45 +0330

    starting of project

---
+++ cmake-build-debug/CMakeFiles/getSmallestPathToDestination.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/getSmallestPathToDestination.dir/main.cpp.obj"
+  "getSmallestPathToDestination.exe"
+  "getSmallestPathToDestination.exe.manifest"
+  "getSmallestPathToDestination.pdb"
+  "libgetSmallestPathToDestination.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/getSmallestPathToDestination.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/getSmallestPathToDestination.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for getSmallestPathToDestination.
+# This may be replaced when dependencies are built.
