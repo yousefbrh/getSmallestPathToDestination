30ff6a0e7a854e7a9b6eba060154c24ebdb2d92f
Yousef Borhani <yousefborhanii@gmail.com>
2020-02-19 15:42:21 +0330

    add some matrix structure

--- main.cpp
+++ main.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+//int getSmallestPathToDestination
+void setMatrixField(int * matrix , int row , int column)
+{
+    for (int i = 0 ; i < column ; i++)
+    {
+        for (int j = 0 ; j < row ; j++)
+        {
 
+        }
+    }
+}
+//void printMatrix
+void setSize(int & rowSet , int & columnSet)
+{
+    cout << "Enter How Many Row You Want? " << endl;
+    cin >> rowSet;
+    cout << "Enter How Many Column You Want? " << endl;
+    cin >> columnSet;
+}
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int row , column;
+    setSize(row , column);
+    int matrixField [row][column];
+    setMatrixField((int *)matrixField , row , column);
     return 0;
 }
