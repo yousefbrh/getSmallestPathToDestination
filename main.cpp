475070d16e1323ecddcdd1cf46fe8a67c1e551cf
Yousef Borhani <yousefborhanii@gmail.com>
2020-02-20 04:48:14 +0330

    setMatrixField and PrintMatrix has been completed

--- main.cpp
+++ main.cpp
@@ -1,17 +1,39 @@
 #include <iostream>
 using namespace std;
 //int getSmallestPathToDestination
+void printMatrix(const int * matrix , int row , int column)
+{
+    for (int i = 0 ; i < column ; i++)
+    {
+        for (int j = 0 ; j < row ; j++)
+        {
+            cout << *((matrix + i * row) + j) << "   ";
+        }
+        cout << endl;
+    }
+}
 void setMatrixField(int * matrix , int row , int column)
 {
     for (int i = 0 ; i < column ; i++)
     {
         for (int j = 0 ; j < row ; j++)
         {
-
+            cout << "This is Column: " << i << " and Row: " << j << " and Enter \"" << 0 << "\" if You Want to Block This Place or Input Some Other Number for Keeping Open: ";
+            int input;
+            cin >> input;
+            if (input == 0)
+            {
+                *((matrix + i * row) + j) = 0;
+            }
+            else
+            {
+                *((matrix + i * row) + j) = 1;
+            }
+            cout << endl;
         }
+        printMatrix((int *)matrix , row , i+1);
     }
 }
-//void printMatrix
 void setSize(int & rowSet , int & columnSet)
 {
     cout << "Enter How Many Row You Want? " << endl;
