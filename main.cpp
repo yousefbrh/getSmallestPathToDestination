e09bb5045a77f9c02ac0519cb68e6b604f640d31
Yousef Borhani <yousefborhanii@gmail.com>
2020-02-22 21:40:46 +0330

    bugfixed on setMatrixField changed whole 2d array to 2d vector write some idea about the getSmallPathToDestination

--- main.cpp
+++ main.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-//int getSmallestPathToDestination
-// ye tabe votodi dorost konam ke column mide on sotono chap kone bad bege kodomo mikhad
+
 
 void setSize(int & rowSet , int & columnSet)
 {
@@ -10,58 +10,54 @@ void setSize(int & rowSet , int & columnSet)
     cout << "Enter How Many Column You Want? " << endl;
     cin >> columnSet;
 }
-void printMatrix(const int * matrix , int column , int row)
+void printMatrix(vector< vector<int> > matrix , int column , int row)
 {
     for (int i = 0 ; i < row ; i++)
     {
         for (int j = 0 ; j < column ; j++)
         {
-            cout << *((matrix + i) + j) << "   ";
+            cout << matrix[i][j] << "   ";
         }
         cout << endl;
     }
 }
-void setMatrixField(int * matrix , int row , int column)
+void setMatrixField(vector< vector<int> > &matrix , int row , int column , int & plus , int & minus)
 {
     for (int i = 0 ; i < row ; i++)
     {
         for (int j = 0 ; j < column ; j++)
         {
-            cout << "This is Column: " << j << " and Row: " << i << " and Enter \"" << 0 << "\" if You Want to Block This Place or Input Some Other Number for Keeping Open: ";
+            cout << "This is Column: " << j << " and Row: " << i  << " and Enter \"" << 0 << "\" if You Want to Block This Place or Input Some Other Number for Keeping Open: ";
             int input;
             cin >> input;
-            cout << *((matrix + 0) + 4) << endl;
             if (input == 0)
             {
-                *((matrix + i) + j) = 0;
+                matrix[i][j] = 0;
+                minus++;
             }
             else
             {
-                *((matrix + i) + j) = 1;
+                matrix[i][j] = 1;
+                plus++;
             }
             cout << endl;
         }
-        printMatrix((int *)matrix , column , i + 1);
+        printMatrix(matrix , column , i + 1);
     }
 }
-void setStartAndEnd(const int * matrix , int & startRow , int & startColumn , int & endRow , int & endColumn , int row , int column)
+void setStartAndEnd(vector< vector<int> > matrix , int & startRow , int & startColumn , int & endRow , int & endColumn)
 {
-    cout << *((matrix + 0) + 4) << endl;
     /// Start
-    while (*((matrix + startColumn) + startRow) == 0)
+    while (matrix[startRow][startColumn] == 0)
     {
         cout << "What Row Do You Want to Start: " << endl;
-        cin >> startColumn;
-        for (int i = 0 ; i < row ; i++)
-        {
-            cout <<  *((matrix + startColumn) + i) << "  ";
-        }
+        cin >> startRow;
         cout << endl;
         cout << "What Column Do You Want to Start: " << endl;
-        cin >> startRow;
-        if (*((matrix + startColumn) + startRow))
+        cin >> startColumn;
+        if (matrix[startRow][startColumn] == 1)
         {
-            cout << "Well Done! You are Starting from ["<< startColumn << "][" << startRow << "] " << endl;
+            cout << "Well Done! You are Starting from ["<< startRow << "][" << startColumn << "] " << endl;
             break;
         }
         else
@@ -70,20 +66,16 @@ void setStartAndEnd(const int * matrix , int & startRow , int & startColumn , in
         }
     }
     /// End
-    while (*((matrix + endColumn) + endRow) == 0)
+    while (matrix[endRow][endColumn] == 0)
     {
         cout << "What Row Do You Want to End: " << endl;
-        cin >> endColumn;
-        for (int i = 0 ; i < row ; i++)
-        {
-            cout <<  *((matrix + endColumn) + i) << "  ";
-        }
+        cin >> endRow;
         cout << endl;
         cout << "What Column Do You Want to End: " << endl;
-        cin >> endRow;
-        if (*((matrix + endColumn) + endRow))
+        cin >> endColumn;
+        if (matrix[endRow][endColumn] == 1)
         {
-            cout << "Well Done! You are Starting from ["<< endColumn << "][" << endRow << "] " << endl;
+            cout << "Well Done! You will Finish at ["<< endRow << "][" << endColumn << "] " << endl;
             break;
         }
         else
@@ -92,12 +84,47 @@ void setStartAndEnd(const int * matrix , int & startRow , int & startColumn , in
         }
     }
 }
+int getSmallestPathToDestination(vector< vector<int> > matrix , int startRow , int startColumn , int endRow , int endColumn , int row , int column , int plus , int minus)
+{
+    cout << "Smallest Path to Destination is : " << endl;
+    int tempRow , tempColumn;
+    int path = 0 ;
+    while (tempRow != endRow && tempColumn != endColumn)
+    {
+        tempRow = startRow;
+        tempColumn = startColumn;
+        if (tempRow < endRow && tempColumn < endColumn)
+        {
+            for (int i = 0; i < plus ; i++)
+            {
+                if(matrix[tempRow][tempColumn] != 0)
+                {
+                    
+                }
+            }
+        }
+        else if (tempRow > endRow && tempColumn < endColumn)
+        {
+
+        }
+        else if (tempRow < endRow && tempColumn > endColumn)
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+}
 int main() {
     int row , column;
     setSize(row , column);
-    int matrixField [row][column];
-    setMatrixField((int *)matrixField , row , column);
+    vector< vector<int> > matrixField (row, vector<int>  (column));
+    int minus = 0 , plus = 0;
+    setMatrixField(matrixField , row , column , plus , minus);
     int startRow = 0 , startColumn = 0 , endRow = 0 , endColumn = 0;
-    setStartAndEnd((int *) matrixField , startRow , startColumn , endRow , endColumn , row , column);
+    setStartAndEnd(matrixField , startRow , startColumn , endRow , endColumn );
+    getSmallestPathToDestination(matrixField , startRow , startColumn , endRow , endColumn , row , column , plus , minus);
     return 0;
 }
